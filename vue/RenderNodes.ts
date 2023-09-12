2a4d665bda5f293a9b2b5d999a6b490c481526fd
formfcw <dev@formfcw.com>
2023-09-12 15:04:55 +0200

    wrap children in function

--- vue/RenderNodes.ts
+++ vue/RenderNodes.ts
@@ -37,7 +37,7 @@ export default defineComponent({
             return renderView<VNode>(
                 content,
                 serializers,
-                (tag, attrs, content) => h(tag, attrs, content as any)
+                (tag, attrs, content) => h(tag, attrs, () => content as any)
             );
         };
     },
